2dc43aec1d5e14f71721b5ff299116a038313eef
nikopapa81 <nikopapa81@gmail.com>
2014-06-06 12:09:03 -0500

    Raghu Fixes

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -8,7 +8,7 @@ validates :avg_exp_days, :presence => true
 has_many :users
 has_many :purchases
 has_many :consumptions
-belongs_to :food_category
-belongs_to :u_of_m
+belongs_to :food_category, :class_name => "FoodCategory", :foreign_key => "category_id"
+belongs_to :u_of_m, :class_name => "UOfM", :foreign_key => "uofm_id"
 
 end
